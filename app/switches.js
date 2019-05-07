d87a92c2998b09f786096d4ce877b17e57a9823f
Connor Kennedy <kennedyconnor02@gmail.com>
2019-05-06 23:48:20 -0600

    tests not working

--- app/switches.js
+++ app/switches.js
@@ -8,51 +8,99 @@
 
 function daysPosition(day, offset) {
     // your code below
+    if (!offset) {
+        switch (day) {
+            case Sunday:
+                return 1;
+                break;
+            case Monday:
+                return 2;
+                break;
+            case Tuesday:
+                return 3;
+                break;
+            case Wednesday:
+                return 4;
+                break;
+            case Thursday:
+                return 5;
+                break;
+            case Friday:
+                return 6;
+                break;
+            case Saturday:
+                return 7;
+                break;
 
-    // your code above
-}
+        }
+    }
+    else {
+        switch (day) {
+            case Sunday:
+                return 7;
+                break;
+            case Monday:
+                return 1;
+                break;
+            case Tuesday:
+                return 2;
+                break;
+            case Wednesday:
+                return 3;
+                break;
+            case Thursday:
+                return 4;
+                break;
+            case Friday:
+                return 5;
+                break;
+            case Saturday:
+                return 6;
+                break;
+        }
+        // your code above
 
 
-// --------------------------------------------
+        // --------------------------------------------
 
 
-/** 2. Given a both a score and a score for par, return the corresponding term:
- *           difference |   term
- *    -----------------------------------
- *  (score == 1) || -3  |   "Ace"
- *            -2        |   "Eagle"
- *            -1        |   "Birdie"
- *             0        |   "Par"
- *            +1        |   "Bogie"
- *            +2        |   "Double Bogie"
- *            >= +3     |   "Ouch"
- */
+        /** 2. Given a both a score and a score for par, return the corresponding term:
+         *           difference |   term
+         *    -----------------------------------
+         *  (score == 1) || -3  |   "Ace"
+         *            -2        |   "Eagle"
+         *            -1        |   "Birdie"
+         *             0        |   "Par"
+         *            +1        |   "Bogie"
+         *            +2        |   "Double Bogie"
+         *            >= +3     |   "Ouch"
+         */
 
-function golfScore(score, par) {
+        function golfScore(score, par) {
 
-}
-
-
-// --------------------------------------------
+        }
 
 
-// 3. Counting Cards. In the casino game Blackjack, a player can gain an advantage over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called card counting.  
-// Having more high cards remaining in the deck favors the player. Each card is assigned a value according to the table below. When the count is positive, the player should bet high. When the count is zero or negative, the player should bet low.
-/**
- *      Value   |   Cards
- *   --------------------------------
- *       +1     |   '2', '3', '4', '5', '6'
- *        0     |	'7', '8', '9' 
- *       -1	    |   '10', 'J', 'Q', 'K','A' 
- */
-// Write a card counting function that will receive a card. The function will increment or decrement the global count variable according to the card's value (see table above). The function will then return the current count and the string "Bet" if the count is positive, or "Hold" if the count is zero or negative.
-// Example:
-// output: '-5 Hold'
-// Example:
-// output: '2 Bet'
+        // --------------------------------------------
 
-let count = 0
 
-function cardCounter(card) {
+        // 3. Counting Cards. In the casino game Blackjack, a player can gain an advantage over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called card counting.  
+        // Having more high cards remaining in the deck favors the player. Each card is assigned a value according to the table below. When the count is positive, the player should bet high. When the count is zero or negative, the player should bet low.
+        /**
+         *      Value   |   Cards
+         *   --------------------------------
+         *       +1     |   '2', '3', '4', '5', '6'
+         *        0     |	'7', '8', '9' 
+         *       -1	    |   '10', 'J', 'Q', 'K','A' 
+         */
+        // Write a card counting function that will receive a card. The function will increment or decrement the global count variable according to the card's value (see table above). The function will then return the current count and the string "Bet" if the count is positive, or "Hold" if the count is zero or negative.
+        // Example:
+        // output: '-5 Hold'
+        // Example:
+        // output: '2 Bet'
 
-}
+        let count = 0
+
+        function cardCounter(card) {
+
+        }
